51cd588a043c219abe616c7f0e1e18e4ea901b8a
awallef <antoine.wallef@gmail.com>
2021-06-28 14:24:20 +0200

    package

--- src/filters/orm-insert.ts
+++ src/filters/orm-insert.ts
@@ -2,8 +2,8 @@ import { plainToClass } from 'class-transformer';
 
 export class OrmConfig
 {
-  save: boolean = true
-  persistBy: string = 'insertOrUpdate'
+  save = true
+  persistBy = 'insertOrUpdate'
   persistOptions?: string = null
 }
 
