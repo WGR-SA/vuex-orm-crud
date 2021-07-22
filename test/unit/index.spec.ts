8bfaa41efa711ada9c46bedd0ddd85aa858b3b53
awallef <antoine.wallef@gmail.com>
2021-07-22 17:24:32 +0200

    Test :)

--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -5,57 +5,31 @@ import { Item, Attr, Str } from '@vuex-orm/core'
 import VuexORMCRUD from '@/index'
 import {Service} from '@/Service'
 
-
 // MOCK
 import mockAxios from 'jest-mock-axios';
-afterEach(() => {
-  // cleaning up the mess left behind the previous test
-  mockAxios.reset();
-});
+afterEach(() => { mockAxios.reset();});
 
 // Vuex ORM
-const baseURL:string = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity'
-const params =  {
-  api_key: "DEMO_KEY",
-  sol: 1000,
-  camera: "fhaz"
-}
-const client = axios.create({
-  baseURL: baseURL,
-  params: params,
-})
-VuexORM.use(VuexORMCRUD, {
-  client
-})
+const client = axios.create()
+VuexORM.use(VuexORMCRUD, {client})
+const store = createStore({plugins: [VuexORM.install()]})
 
-const store = createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-  plugins: [VuexORM.install()]
-})
+// create user
+class Photo extends VuexORM.Model {
+
+  static entity = 'photos'
+
+  @Attr(null)
+  id!: number | null
+
+  @Str('')
+  img_src!: string
+}
 
 // TEST BABY
 describe('unit/VuexORMCRUD', () => {
 
-  // create user
-  class Photo extends VuexORM.Model {
-
-    static entity = 'photos'
-
-    @Attr(null)
-    id!: number | null
-
-    @Str('')
-    img_src!: string
-  }
-
-  // INSTALL
+  // 1. INSTALL
   it('can install the plugin', async () => {
 
     // check AXIOS
@@ -66,46 +40,34 @@ describe('unit/VuexORMCRUD', () => {
     expect(photoRepo.$crud).toBeInstanceOf(Service)
   })
 
-
-  // GET
+  // 2. GET
   it('can DO GET', async () => {
 
     // check get
     const photoRepo = store.$repo(Photo)
     const promise = photoRepo.$crud.get()
 
-    //expect(mockAxios.get).toHaveBeenCalledWith(baseURL, params);
+    // mock response
     mockAxios.mockResponse({  "data": [
-      {
-        "id": 1,
-        "img_src": "A.JPG",
-      },
-      {
-        "id": 2,
-        "img_src": "B.JPG",
-      }
+      {"id": 1,"img_src": "A.JPG"},
+      {"id": 2,"img_src": "B.JPG"}
     ]});
     await promise;
 
     expect(photoRepo.orderBy('id').first()).toBeInstanceOf(Photo)
   })
 
-  // SAVE
+  // 3. SAVE
   it('can DO SAVE', async () => {
 
     // check get
     const photoRepo = store.$repo(Photo)
-    const promise = photoRepo.$crud.save({
-      "id": 3,
-      "img_src": "C.JPG",
-    })
+    const promise = photoRepo.$crud.save({"id": 3,"img_src": "C.JPG"})
 
-    mockAxios.mockResponse({ "data": {
-      "id": 3,
-      "img_src": "C.JPG",
-    }});
-
+    // mock response
+    mockAxios.mockResponse({ "data": {"id": 3, "img_src": "C.JPG"}});
     await promise;
+
     const lastPhoto:Item<Photo> = photoRepo.orderBy('id', 'desc').first()
     expect(lastPhoto).toBeInstanceOf(Photo)
     expect(lastPhoto?.id).toBe(3)
